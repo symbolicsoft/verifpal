0e4ab438928140a8b0ecdacd279e48549f8dbd72
Nadim Kobeissi <nadim@symbolic.software>
2019-09-08 16:35:04 +0200

    Correct copyright.

--- internal/app/verifpal/main.go
+++ internal/app/verifpal/main.go
@@ -35,7 +35,7 @@ func mainParse(filename string) (*verifpal, *knowledgeMap, []*principalState) {
 func main() {
 	fmt.Fprint(os.Stdout, fmt.Sprintf("%s%s%s%s%s\n%s\n%s\n\n",
 		"Verifpal ", mainVersion, " (", runtime.Version(), ")",
-		"© 2019 Symbolic Software — https://verifpal.com",
+		"© 2019 Nadim Kobeissi — https://verifpal.com",
 		"WARNING: Verifpal is experimental software.",
 	))
 	if len(os.Args) != 3 {
