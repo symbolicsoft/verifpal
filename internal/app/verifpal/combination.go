e1d6c24de36110dc66ffe7e89c0d655ee607f00a
Nadim Kobeissi <nadim@symbolic.software>
2019-09-29 16:12:18 +0200

    Fix non-terminating analysis in case of no replacement combination.

--- internal/app/verifpal/combination.go
+++ internal/app/verifpal/combination.go
@@ -6,19 +6,23 @@ package main
 
 func (valReplacementMap *replacementMap) combinationNext() bool {
 	lastCombination := false
-	for i := 0; i < len(valReplacementMap.combination); i++ {
-		valReplacementMap.combination[i] = valReplacementMap.replacements[i][valReplacementMap.depthIndex[i]]
-		valReplacementMap.requiredKnown[i] = valReplacementMap.requiredKnowns[i][valReplacementMap.depthIndex[i]]
-		if i == len(valReplacementMap.combination)-1 {
-			valReplacementMap.depthIndex[i] = valReplacementMap.depthIndex[i] + 1
-			for ii := i; ii >= 0; ii-- {
-				if valReplacementMap.depthIndex[ii] == len(valReplacementMap.replacements[ii]) {
-					if ii > 0 {
-						valReplacementMap.depthIndex[ii] = 0
-						valReplacementMap.depthIndex[ii-1] = valReplacementMap.depthIndex[ii-1] + 1
-					} else {
-						lastCombination = true
-						break
+	if len(valReplacementMap.combination) == 0 {
+		lastCombination = true
+	} else {
+		for i := 0; i < len(valReplacementMap.combination); i++ {
+			valReplacementMap.combination[i] = valReplacementMap.replacements[i][valReplacementMap.depthIndex[i]]
+			valReplacementMap.requiredKnown[i] = valReplacementMap.requiredKnowns[i][valReplacementMap.depthIndex[i]]
+			if i == len(valReplacementMap.combination)-1 {
+				valReplacementMap.depthIndex[i] = valReplacementMap.depthIndex[i] + 1
+				for ii := i; ii >= 0; ii-- {
+					if valReplacementMap.depthIndex[ii] == len(valReplacementMap.replacements[ii]) {
+						if ii > 0 {
+							valReplacementMap.depthIndex[ii] = 0
+							valReplacementMap.depthIndex[ii-1] = valReplacementMap.depthIndex[ii-1] + 1
+						} else {
+							lastCombination = true
+							break
+						}
 					}
 				}
 			}
