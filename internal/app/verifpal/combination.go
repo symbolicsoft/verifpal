7be70677c7e0bef24c5d63e126aa0480d6ad4b48
Nadim Kobeissi <nadim@symbolic.software>
2019-10-16 19:28:03 +0200

    Small cleanup.

--- internal/app/verifpal/combination.go
+++ internal/app/verifpal/combination.go
@@ -8,23 +8,24 @@ func (valReplacementMap *replacementMap) combinationNext() bool {
 	lastCombination := false
 	if len(valReplacementMap.combination) == 0 {
 		lastCombination = true
-	} else {
-		for i := 0; i < len(valReplacementMap.combination); i++ {
-			valReplacementMap.combination[i] = valReplacementMap.replacements[i][valReplacementMap.depthIndex[i]]
-			if i == len(valReplacementMap.combination)-1 {
-				valReplacementMap.depthIndex[i] = valReplacementMap.depthIndex[i] + 1
-				valReplacementMap.lastIncrement = i
-				for ii := i; ii >= 0; ii-- {
-					if valReplacementMap.depthIndex[ii] == len(valReplacementMap.replacements[ii]) {
-						if ii > 0 {
-							valReplacementMap.depthIndex[ii] = 0
-							valReplacementMap.depthIndex[ii-1] = valReplacementMap.depthIndex[ii-1] + 1
-							valReplacementMap.lastIncrement = ii - 1
-						} else {
-							lastCombination = true
-							break
-						}
-					}
+		return lastCombination
+	}
+	for i := 0; i < len(valReplacementMap.combination); i++ {
+		valReplacementMap.combination[i] = valReplacementMap.replacements[i][valReplacementMap.depthIndex[i]]
+		if i != len(valReplacementMap.combination)-1 {
+			continue
+		}
+		valReplacementMap.depthIndex[i] = valReplacementMap.depthIndex[i] + 1
+		valReplacementMap.lastIncrement = i
+		for ii := i; ii >= 0; ii-- {
+			if valReplacementMap.depthIndex[ii] == len(valReplacementMap.replacements[ii]) {
+				if ii > 0 {
+					valReplacementMap.depthIndex[ii] = 0
+					valReplacementMap.depthIndex[ii-1] = valReplacementMap.depthIndex[ii-1] + 1
+					valReplacementMap.lastIncrement = ii - 1
+				} else {
+					lastCombination = true
+					break
 				}
 			}
 		}
