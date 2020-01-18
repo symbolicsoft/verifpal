a25df56527d7d78286a06d7dd248475c3dcce8b5
Nadim Kobeissi <nadim@symbolic.software>
2020-01-18 17:05:17 +0100

    More oncurrency!

---
+++ internal/verifpal/verifyresults.go
@@ -0,0 +1,55 @@
+/* SPDX-FileCopyrightText: © 2019-2020 Nadim Kobeissi <nadim@symbolic.software>
+ * SPDX-License-Identifier: GPL-3.0-only */
+// 00000000000000000000000000000000
+
+package verifpal
+
+import "strings"
+
+var verifyResultsReady chan bool = make(chan bool)
+var verifyResultsReads chan verifyResultsRead = make(chan verifyResultsRead)
+var verifyResultsWrites chan verifyResultsWrite = make(chan verifyResultsWrite)
+
+func verifyResultsInit(m Model) bool {
+	go func() {
+		verifyResults := []VerifyResult{}
+		for _, q := range m.queries {
+			verifyResults = append(verifyResults, VerifyResult{
+				query:    q,
+				resolved: false,
+				summary:  "",
+			})
+		}
+		verifyResultsReady <- true
+		for {
+			select {
+			case read := <-verifyResultsReads:
+				read.resp <- verifyResults
+			case write := <-verifyResultsWrites:
+				qw := prettyQuery(write.verifyResult.query)
+				for i, verifyResult := range verifyResults {
+					qv := prettyQuery(verifyResult.query)
+					if strings.Compare(qw, qv) == 0 {
+						verifyResults[i].resolved = write.verifyResult.resolved
+						verifyResults[i].summary = write.verifyResult.summary
+					}
+				}
+				write.resp <- true
+			}
+		}
+	}()
+	return <-verifyResultsReady
+}
+
+func verifyResultsGetRead() []VerifyResult {
+	read := verifyResultsRead{
+		resp: make(chan []VerifyResult),
+	}
+	verifyResultsReads <- read
+	return <-read.resp
+}
+
+func verifyResultsPutWrite(write verifyResultsWrite) bool {
+	verifyResultsWrites <- write
+	return <-write.resp
+}
