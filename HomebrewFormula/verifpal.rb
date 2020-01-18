30b5872eb529b706cc94eb8ef8457b019a4dce53
Nadim Kobeissi <nadim@symbolic.software>
2020-01-18 21:21:57 +0100

    Update Homebrew formula for Verifpal 0.9.1.

--- HomebrewFormula/verifpal.rb
+++ HomebrewFormula/verifpal.rb
@@ -4,8 +4,8 @@
 class Verifpal < Formula
 	desc "Cryptographic protocol analysis for students and engineers"
 	homepage "https://verifpal.com"
-	url "https://source.symbolic.software/verifpal/verifpal/archive/v0.9.0.zip"
-	sha256 "1526cce3982b9a4661156c72dd30cdbd523af99c738ecd7820edf107615e8194"
+	url "https://source.symbolic.software/verifpal/verifpal/archive/v0.9.1.zip"
+	sha256 ""
 
 	depends_on "go" => :build
 
