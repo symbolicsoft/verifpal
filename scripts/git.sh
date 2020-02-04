cba16f5efc74d3724e582ac00c198110e2a00fbe
Nadim Kobeissi <nadim@symbolic.software>
2020-02-04 11:13:04 +0100

    Move git prep commands to separate script

---
+++ scripts/git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: © 2019-2020 Nadim Kobeissi <nadim@symbolic.software>
+# SPDX-License-Identifier: GPL-3.0-only  
+  
+mkdir -p $HOME/.ssh
+echo "$GIT_PUSH_SSH_KEY" > $HOME/.ssh/id_ed25519
+chmod -R 700 $HOME/.ssh
+ssh-keyscan source.symbolic.software > $HOME/.ssh/known_hosts
+git config --global user.name "Drone"
+git config --global user.email "drone@drone.symbolic.software"
