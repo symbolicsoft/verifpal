71b487b43dc06db4c2a76d89fa00110ac3615c72
Nadim Kobeissi <nadim@symbolic.software>
2020-02-07 21:01:27 +0000

    Update package manager formulas [CI SKIP]

--- HomebrewFormula/verifpal.rb
+++ HomebrewFormula/verifpal.rb
@@ -4,8 +4,8 @@
 class Verifpal < Formula
 	desc "Cryptographic protocol analysis for students and engineers"
 	homepage "https://verifpal.com"
-	url "https://source.symbolic.software/verifpal/verifpal/archive/v0.10.5.zip"
-	sha256 "46ca2a5bf57ddb40a608ff72b4371e021bf6c92ce28253d27e3d7955c1619ea7"
+	url "https://source.symbolic.software/verifpal/verifpal/archive/v0.10.6.zip"
+	sha256 "6a157e7c17baaaf11f3a0f108d49b4ead0f8f3afe262aad2bd69cdb6a09c8ccd"
 
 	depends_on "go" => :build
 
