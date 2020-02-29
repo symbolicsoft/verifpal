58a04483c1bae4e99a996a6c1f9deab31102404d
Nadim Kobeissi <nadim@symbolic.software>
2020-02-29 14:57:34 +0100

    Snap script [CI SKIP]

---
+++ scripts/snap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: © 2019-2020 Nadim Kobeissi <nadim@symbolic.software>
+# SPDX-License-Identifier: GPL-3.0-only
+
+snapcraft clean
+snapcraft
+snapcraft push --release=stable *.snap
+rm *.snap
