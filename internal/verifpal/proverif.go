45fbd1d37893bed5e0f1b82f3aba6ccb9ad7dd41
Nadim Kobeissi <nadim@symbolic.software>
2020-03-23 13:19:08 +0100

    Add proverif.go

---
+++ internal/verifpal/proverif.go
@@ -0,0 +1,9 @@
+/* SPDX-FileCopyrightText: © 2019-2020 Nadim Kobeissi <nadim@symbolic.software>
+ * SPDX-License-Identifier: GPL-3.0-only */
+// 616bf0023a90ab68ba9e693bf9994779
+
+package verifpal
+
+func proverif(m Model) {
+
+}
