df952c0156f6a6a82d1625b33445de46f1168bfc
ilmu <ilmu@rishi.is>
2020-04-27 18:39:32 +0000

    Add nix derivation

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let unstable = import (fetchTarball https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz) { };
+
+in unstable.buildGoPackage rec {
+  pname = "verifpal";
+  version = "latest";
+
+  goPackagePath = "verifpal.com";
+
+  src = ./.;
+  goDeps = ./nix/deps.nix;
+
+  nativeBuildInputs = [ pigeon ];
+
+  postPatch = ''
+    sed -e 's|/bin/echo |echo |g' -i Makefile
+  '';
+
+  buildPhase = ''
+    make -C go/src/$goPackagePath parser linux
+  '';
+
+  installPhase = ''
+    mkdir -p $bin/bin
+    cp go/src/$goPackagePath/build/linux/verifpal $bin/bin/
+  '';
+
+}
---
+++ nix/deps.nix
@@ -0,0 +1,39 @@
+# file generated from go.mod using vgo2nix (https://github.com/adisbladis/vgo2nix)
+[
+  {
+    goPackagePath = "github.com/akavel/rsrc";
+    fetch = {
+      type = "git";
+      url = "https://github.com/akavel/rsrc";
+      rev = "v0.8.0";
+      sha256 = "0kq4c5rf5qrlkgd0jwlh1wih8r9194ddnmvzcn2ki6s55w000gi7";
+    };
+  }
+  {
+    goPackagePath = "github.com/josephspurrier/goversioninfo";
+    fetch = {
+      type = "git";
+      url = "https://github.com/josephspurrier/goversioninfo";
+      rev = "14b0ab84c6ca";
+      sha256 = "10d5xhf0m4w8rlmbds8h7r54xrj3i6zcxjy35675jcbpjby8z1h1";
+    };
+  }
+  {
+    goPackagePath = "github.com/logrusorgru/aurora";
+    fetch = {
+      type = "git";
+      url = "https://github.com/logrusorgru/aurora";
+      rev = "e9ef32dff381";
+      sha256 = "19laya9dav84miw3d0c9vgiv577wzrhydv5mdiii59sgbd780hhq";
+    };
+  }
+  {
+    goPackagePath = "github.com/mna/pigeon";
+    fetch = {
+      type = "git";
+      url = "https://github.com/mna/pigeon";
+      rev = "v1.0.0";
+      sha256 = "120m90lig5984wd2k7x2fclx5vb64lxmx4j3065pp0q1fqjif87z";
+    };
+  }
+]
