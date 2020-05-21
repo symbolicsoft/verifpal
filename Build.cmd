898128458f6056fedc8a5a04f555781127ea8f74
Nadim Kobeissi <nadim@symbolic.software>
2020-05-21 10:07:33 +0200

    Add -trimpath to go build parameters
    This will facilitate reproducible builds.

--- Build.cmd
+++ Build.cmd
@@ -22,28 +22,28 @@
 @setx GOOS "" >nul
 @go generate verifpal.com/cmd/verifpal
 @setx GOOS "windows" >nul
-@go build -gcflags="-e" -ldflags="-s -w" -o build\windows verifpal.com/cmd/verifpal
+@go build -trimpath -gcflags="-e" -ldflags="-s -w" -o build\windows verifpal.com/cmd/verifpal
 @echo  OK
 
 @echo|set /p="[Verifpal] Building Verifpal for Linux..."
 @setx GOOS "" >nul
 @go generate verifpal.com/cmd/verifpal
 @setx GOOS "linux" >nul
-@go build -gcflags="-e" -ldflags="-s -w" -o build\linux verifpal.com/cmd/verifpal
+@go build -trimpath -gcflags="-e" -ldflags="-s -w" -o build\linux verifpal.com/cmd/verifpal
 @echo    OK
 
 @echo|set /p="[Verifpal] Building Verifpal for macOS..."
 @setx GOOS "" >nul
 @go generate verifpal.com/cmd/verifpal
 @setx GOOS "darwin" >nul
-@go build -gcflags="-e" -ldflags="-s -w" -o build\macos verifpal.com/cmd/verifpal
+@go build -trimpath -gcflags="-e" -ldflags="-s -w" -o build\macos verifpal.com/cmd/verifpal
 @echo    OK
 
 @echo|set /p="[Verifpal] Building Verifpal for FreeBSD..."
 @setx GOOS "" >nul
 @go generate verifpal.com/cmd/verifpal
 @setx GOOS "freebsd" >nul
-@go build -gcflags="-e" -ldflags="-s -w" -o build\freebsd verifpal.com/cmd/verifpal
+@go build -trimpath -gcflags="-e" -ldflags="-s -w" -o build\freebsd verifpal.com/cmd/verifpal
 @echo  OK
 
 @echo|set /p="[Verifpal] Cleaning up..."
