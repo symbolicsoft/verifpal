cb8c9cef2dc80cc2241861119220ded311e4d3d5
drone <drone@symbolic.software>
2020-07-19 15:44:19 +0000

    Brew formula update for verifpal version v0.17.5

--- HomebrewFormula/verifpal.rb
+++ HomebrewFormula/verifpal.rb
@@ -2,16 +2,16 @@
 class Verifpal < Formula
   desc "Cryptographic protocol analysis for students and engineers."
   homepage "https://verifpal.com"
-  version "0.17.4"
+  version "0.17.5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://source.symbolic.software/verifpal/verifpal/uploads/88ec6dbe188251b5498b126488ce3998/verifpal_0.17.4_macos_amd64.zip"
-    sha256 "886633b72d57e617085202be7a3ae1568ccec73e82fad66ccd08051a07322a3e"
+    url "https://source.symbolic.software/verifpal/verifpal/uploads/4fe098f6de1974259e4e85a11701a30b/verifpal_0.17.5_macos_amd64.zip"
+    sha256 "7d194cb2e8d94a8f73c0ccc897e9d045171d6a68a7dc723c3b72c51e681e2c4a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://source.symbolic.software/verifpal/verifpal/uploads/cf3b1a56228a4046052ce695168eda52/verifpal_0.17.4_linux_amd64.zip"
-      sha256 "f542aa87d805f1de269e43caec970cf588969f7f6567ce3044668d7a730de621"
+      url "https://source.symbolic.software/verifpal/verifpal/uploads/31a23b8a1717ec61e760e0b78a2881c2/verifpal_0.17.5_linux_amd64.zip"
+      sha256 "8906ba88304f4c47a5b25fadaa50fb2217ba54750343c3773e301598159ff262"
     end
   end
 
